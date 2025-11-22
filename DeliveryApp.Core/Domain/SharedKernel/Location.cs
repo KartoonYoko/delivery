ed01c0472789a0b333e2c4dffe1bcc06c179d46a
kyoko <crashed9001@gmail.com>
2025-11-22 13:57:10 +0300

    added location value object and it's unit tests

---
+++ DeliveryApp.Core/Domain/SharedKernel/Location.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel;
+
+public record Location
+{
+    public int X { get; }
+
+    public int Y { get; }
+
+    private Location()
+    {
+    }
+
+    private Location(int x, int y) : this()
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Result<Location, Error> Create(int x, int y)
+    {
+        if (x < 1 || x > 10) return GeneralErrors.ValueIsRequired(nameof(x));
+        if (y < 1 || y > 10) return GeneralErrors.ValueIsRequired(nameof(y));
+
+        return new Location(x, y);
+    }
+
+    public static Location CreateRandom()
+    {
+        var random = new Random();
+
+        var x = random.Next(1, 10);
+        var y = random.Next(1, 10);
+
+        return new Location(x, y);
+    }
+
+    public int DistanceTo(Location target)
+    {
+        return Math.Abs(target.X - X) + Math.Abs(target.Y - Y);
+    }
+}
