860f98dd2dbea0c98e2ccdf612eef01b582363e1
kyoko <crashed9001@gmail.com>
2025-12-16 18:44:52 +0300

    removed unnecessary queries; now queries are placed in infrastructure

--- DeliveryApp.Core/Application/Queries/GetAllCouriers/GetAllCouriersQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using DeliveryApp.Core.Ports;
-
-namespace DeliveryApp.Core.Application.Queries.GetAllCouriers;
-
-public class GetAllCouriersQuery(IGetAllCouriersQuery query)
-{
-    public Task<GetCouriersResponse> HandleAsync(CancellationToken cancellationToken = default)
-        => query.Handle(cancellationToken);
-}
--- DeliveryApp.Core/Application/Queries/GetAllUncompletedOrders/GetAllUncompletedOrdersQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using DeliveryApp.Core.Ports;
-
-namespace DeliveryApp.Core.Application.Queries.GetAllUncompletedOrders;
-
-public class GetAllUncompletedOrdersQuery(IGetAllUncompletedOrdersQuery query)
-{
-    public Task<GetUncompletedOrdersResponse> HandleAsync(CancellationToken cancellationToken = default)
-        => query.Handle(cancellationToken);
-}
